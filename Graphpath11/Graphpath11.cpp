8efa1582b3849c48010e90f3becedff0ab8400a4
ewent <ewent@LAPTOP-23S3E4HC>
2023-12-23 23:54:43 +0300

    Добавьте файлы проекта.

---
+++ Graphpath11/Graphpath11.cpp
@@ -0,0 +1,51 @@
+﻿// Graphpath11.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+// A utility function to add an edge in an
+// undirected graph.
+void addEdge(vector<int> adj[], int u, int v)
+{
+	adj[u].push_back(v);
+	adj[v].push_back(u);
+}
+// A utility function to print the adjacency list
+// representation of graph
+void printGraph(vector<int> adj[], int V)
+{
+	for (int v = 0; v < V; ++v) {
+		cout << "\n Adjacency list of vertex " << v
+			<< "\n head ";
+		for (auto x : adj[v])
+			cout << "-> " << x;
+		printf("\n");
+	}
+}
+// Driver code
+int main()
+{
+	const int V = 5;
+	vector<int> adj[V];
+	addEdge(adj, 0, 1);
+	addEdge(adj, 0, 4);
+	addEdge(adj, 1, 2);
+	addEdge(adj, 1, 3);
+	addEdge(adj, 1, 4);
+	addEdge(adj, 2, 3);
+	addEdge(adj, 3, 4);
+	printGraph(adj, V);
+	return 0;
+}
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
